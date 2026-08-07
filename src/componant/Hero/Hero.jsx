ca6bcbbb52339d195dea27c2e3e1a5cd27879744
Sunil Shelke <shelkesunil072@gmail.com>
2026-08-07 17:32:51 +0530

    hero

--- src/componant/Hero/Hero.jsx
+++ src/componant/Hero/Hero.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
-
+import "./Hero.scss"
 const Hero = () => {
-  return (
-    <>
-      <div className="hero-parent parent">
-        <div className="hero-cont cont">
-            
-        </div>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className="hero-parent parent">
+                <div className="hero-cont cont">
+                    <div className="hero-left">
+
+                    </div>
+                    <div className="hero-right">
+                        <div className="content-box">
+                            <div className="heading">Elegant homes for sale</div>
+                            <div className="desc">There are many variations of passages of lorem ipsum available, but the majority have suffered alteration in some form by injected humour or randomised words which don't look even slightly believable.</div>
+                        </div>
+                        <div className="price-group">
+                            PRICE STARTS AT <span>
+                                l;
+
+
+                                
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Hero
